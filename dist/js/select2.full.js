616a559f83bc0abb29baa2221de682858ed77031
Charles Huang <Charles.Huang@iress.com.au>
2015-07-10 20:25:17 +1000

    adding compiled version

--- dist/js/select2.full.js
+++ dist/js/select2.full.js
@@ -731,6 +731,10 @@ S2.define('select2/utils',[
     $element.append($nodes);
   };
 
+    Utils.capitalize = function (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
   return Utils;
 });
 
@@ -3298,6 +3302,125 @@ S2.define('select2/data/array',[
   return ArrayAdapter;
 });
 
+S2.define('select2/data/hint',[
+    './select',
+    '../utils',
+    'jquery'
+], function (SelectAdapter, Utils, $) {
+    function HintAdapter ($element, options) {
+        HintAdapter.__super__.constructor.call(this, $element, options);
+    }
+
+    Utils.Extend(HintAdapter, SelectAdapter);
+
+    HintAdapter.prototype.select = function (data) {
+        var self = this;
+
+        data.selected = true;
+
+        // If data.element is a DOM node, use it instead
+        if ($(data.element).is('option')) {
+            data.element.selected = true;
+
+            this.$element.trigger('change');
+
+            return;
+        }
+
+        if (this.$element.prop('multiple')) {
+            this.current(function (currentData) {
+                var val = [];
+
+                data = [data];
+                data.push.apply(data, currentData);
+
+                for (var d = 0; d < data.length; d++) {
+                    var id = data[d].id;
+
+                    if ($.inArray(id, val) === -1) {
+                        val.push(id);
+                    }
+                }
+
+                self.$element.val(val);
+                self.$element.trigger('change');
+            });
+        } else {
+            var val = data.id;
+
+            this.$element.val(val);
+            this.$element.trigger('change');
+        }
+    };
+
+    HintAdapter.prototype.unselect = function (data) {
+        var self = this;
+
+        if (!this.$element.prop('multiple')) {
+            return;
+        }
+
+        data.selected = false;
+
+        if ($(data.element).is('option')) {
+            data.element.selected = false;
+
+            this.$element.trigger('change');
+
+            return;
+        }
+
+        this.current(function (currentData) {
+            var val = [];
+
+            for (var d = 0; d < currentData.length; d++) {
+                var id = currentData[d].id;
+
+                if (id !== data.id && $.inArray(id, val) === -1) {
+                    val.push(id);
+                }
+            }
+
+            self.$element.val(val);
+
+            self.$element.trigger('change');
+        });
+    };
+
+    HintAdapter.prototype.query = function (params, callback) {
+        var data = [];
+        var self = this;
+
+        var $options = this.$element.children();
+
+        $options.each(function () {
+            var $option = $(this);
+
+            if (!$option.is('option') && !$option.is('optgroup')) {
+                return;
+            }
+
+            var option = self.item($option);
+            if (params && params.term) {
+                $.each(option.children, function (i, m) {
+                    m.text = Utils.capitalize(m.id) + ': ' + params.term;
+                });
+            }
+            data.push(option);
+        });
+
+        callback({
+            results: data
+        });
+    };
+
+    HintAdapter.prototype.matches = function (params, data) {
+        return data;
+    };
+
+    return HintAdapter;
+});
+
 S2.define('select2/data/ajax',[
   './array',
   '../utils',
@@ -3818,23 +3941,28 @@ S2.define('select2/dropdown/search',[
     });
   };
 
-  Search.prototype.handleSearch = function (evt) {
-    if (!this._keyUpPrevented) {
-      var input = this.$search.val();
+    Search.prototype.beforeHandleSearch = function (evt) {
+        console.log(this.$search);
+    };
 
-      this.trigger('query', {
-        term: input
-      });
-    }
+    Search.prototype.handleSearch = function (evt) {
+        this.beforeHandleSearch(evt);
+        if (!this._keyUpPrevented) {
+            var input = this.$search.val();
 
-    this._keyUpPrevented = false;
-  };
+            this.trigger('query', {
+                term: input
+            });
+        }
 
-  Search.prototype.showSearch = function (_, params) {
-    return true;
-  };
+        this._keyUpPrevented = false;
+    };
 
-  return Search;
+    Search.prototype.showSearch = function (_, params) {
+        return true;
+    };
+
+    return Search;
 });
 
 S2.define('select2/dropdown/hidePlaceholder',[
@@ -4337,7 +4465,8 @@ S2.define('select2/defaults',[
   './diacritics',
 
   './data/select',
-  './data/array',
+    './data/array',
+    './data/hint',
   './data/ajax',
   './data/tags',
   './data/tokenizer',
@@ -4364,7 +4493,7 @@ S2.define('select2/defaults',[
 
              Utils, Translation, DIACRITICS,
 
-             SelectData, ArrayData, AjaxData, Tags, Tokenizer,
+             SelectData, ArrayData, HintData, AjaxData, Tags, Tokenizer,
              MinimumInputLength, MaximumInputLength, MaximumSelectionLength,
 
              Dropdown, DropdownSearch, HidePlaceholder, InfiniteScroll,
@@ -4382,7 +4511,9 @@ S2.define('select2/defaults',[
       if (options.ajax != null) {
         options.dataAdapter = AjaxData;
       } else if (options.data != null) {
-        options.dataAdapter = ArrayData;
+          options.dataAdapter = ArrayData;
+      } else if (options.hint != null) {
+          options.dataAdapter = HintData;
       } else {
         options.dataAdapter = SelectData;
       }
